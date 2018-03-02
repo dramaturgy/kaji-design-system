30cf2eaa709c78020021c9fc975e122a52bf071c
dramaturgy <advanceddramaturgy@gmail.com>
2018-03-03 01:54:35 +0700

    major: remove tuition performance chart

--- mvp-cms/src/copy/sample/chart/test-tuition.js
+++ mvp-cms/src/copy/sample/chart/test-tuition.js
@@ -1,67 +1,4 @@
 $(function () {
-  // Chart test
-  function generateChartPerfomance() {
-    var _fakeLabels = {
-      months: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-      grades: ['Grade 1', 'Grade 2', 'Grade 3'] }
-    ,   _fakeData = [65, 59, 80];
-
-    var chart = new Chart(document.getElementById('js_tuition_chart_performance').getContext('2d'), {
-      type: 'horizontalBar',
-      data: {
-        labels: _fakeLabels.grades,
-        datasets: [{
-          label: "Payment Performance",
-          data: _fakeData,
-          fill: false,
-          backgroundColor: 'hsla(275, 51%, 80%, 1)'
-        }]
-      },
-      options: {
-        responsive: true,
-        maintainAspectRatio: false,
-        legend: {
-          display: false
-        },
-        scales: {
-          xAxes: [{
-            gridLines: {
-              display: false
-            },
-            ticks: {
-              beginAtZero: true,
-              max: 100,
-              fontSize: '10',
-              fontColor: 'rgba(117, 102, 127, .8)'
-            }
-          }],
-          yAxes: [{
-            height: 60,
-            categoryPercentage: 1,
-            barPercentage: 1,
-            maxBarThickness: 16,
-            gridLines: {
-              drawBorder: false,
-            },
-            ticks: {
-              fontSize: '12',
-              fontColor: 'rgba(68, 60, 74, .8)',
-              fontFamily: '-apple-system, BlinkMacSystemFont, "Segoe UI", "Helvetica Neue", Arial, sans-serif'
-            }
-          }]
-        },
-        tooltips: {
-          backgroundColor: 'rgba(117, 102, 127, 1)',
-          cornerRadius: 3,
-          displayColors: false,
-          titleFontFamily: '-apple-system, BlinkMacSystemFont, "Segoe UI", "Helvetica Neue", Arial, sans-serif',
-          titleMarginBottom: 8,
-          bodyFontFamily: '-apple-system, BlinkMacSystemFont, "Segoe UI", "Helvetica Neue", Arial, sans-serif'
-        }
-      }
-    });
-  }
-
   function generateChartOverall() {
     var _fakeLabels = {
       months: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
@@ -101,6 +38,5 @@ $(function () {
     });
   }
 
-  generateChartPerfomance();
   generateChartOverall();
 });
